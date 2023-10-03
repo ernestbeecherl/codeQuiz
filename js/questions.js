68d7640a2ddecc4803b1c631aa5622162604d49b
Ernest Beecherl <ernestbeecherl98@gmail.com>
2023-10-03 16:21:30 -0500

    first commit for hw

---
+++ js/questions.js
@@ -0,0 +1,34 @@
+var questions = [
+
+    { 
+        Question: "Question number one", 
+
+        Choice: ["A", "B", "C"  ], 
+
+        Answer: ["B"], 
+    }, 
+    
+
+
+    { 
+    Question: "Question number two", 
+
+    Choice: ["A", "B", "C"  ], 
+
+    Answer: ["C"],
+
+    }, 
+
+
+    { 
+        Question: "Question number three", 
+    
+        Choice: ["A", "B", "C"  ], 
+    
+        Answer: ["A"],
+    
+    }, 
+
+
+
+]
